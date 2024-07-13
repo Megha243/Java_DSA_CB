3051b39233debc13b5908f0d26a8657aa508b7f2
Megha Gupta <meghagupta2019875@gmail.com>
2024-07-13 12:35:38 +0530

    first commit

---
+++ Week0/Day1/Flowchart.java
@@ -0,0 +1,12 @@
+package Week0.Day1;
+
+import java.util.Scanner;
+
+public class Flowchart{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("Ans: "+a);
+        sc.close();
+    }
+}
